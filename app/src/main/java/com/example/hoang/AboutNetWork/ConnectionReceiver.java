6399522a9c2a13883065c707c37e324ce65b53b6
NguyenVanLeNguyen <nguyenphihoangb@gmail.com>
2017-10-11 07:09:14 +0700

    week4_day3

---
+++ app/src/main/java/com/example/hoang/AboutNetWork/ConnectionReceiver.java
@@ -0,0 +1,26 @@
+package com.example.hoang.AboutNetWork;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+/**
+ * Created by hoang on 11/10/2017.
+ */
+
+public class ConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+    public static boolean isConnected() {
+        ConnectivityManager
+                cm = (ConnectivityManager) WifiApp.getInstance().getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return ni != null && ni.isConnectedOrConnecting();
+    }
+
+}
---
+++ app/src/main/java/com/example/hoang/AboutNetWork/WifiApp.java
@@ -0,0 +1,21 @@
+package com.example.hoang.AboutNetWork;
+
+import android.app.Application;
+
+/**
+ * Created by hoang on 11/10/2017.
+ */
+
+public class WifiApp extends Application {
+    static WifiApp wifiInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        wifiInstance = this;
+    }
+
+    public static synchronized WifiApp getInstance() {
+        return wifiInstance;
+    }
+}
